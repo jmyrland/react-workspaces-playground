97c9f19186fcba06c3f9fb04804f6512c7be3dc8
Jørn André Myrland <j@myrland.nu>
2020-01-16 09:43:24 +0100

    Changes to reproduce issue #29

--- packages/components-typescript/src/CompOne/CompOne.tsx
+++ packages/components-typescript/src/CompOne/CompOne.tsx
@@ -1,14 +1,23 @@
 import React from 'react';
 import './CompOne.css';
 
+class Test {
+  prop: string;
+  constructor(v: string) {
+    this.prop = v;
+  }
+}
+
 const CompOne: React.FC = () => {
+  const test = new Test('hey');
+
   return (
     <div className="Comp">
       <h3>
         <span role="img" aria-label="React Logo">
           ⚛️
         </span>
-          Comp One
+          {test.prop as string}
       </h3>
     </div>
   );
